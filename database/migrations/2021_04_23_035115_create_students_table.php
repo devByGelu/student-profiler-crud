2772550afe216c1a68a7723d3f3b137821327c16
Angelu <200610313@my.xu.edu.ph>
2021-04-23 11:53:23 +0800

    Created model & migration for student

---
+++ database/migrations/2021_04_23_035115_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->primary('idNumber');
+            $table->bigInteger('idNumber');
+            $table->integer('slmisNumber');
+            $table->string('sex');
+            $table->string('fName');
+            $table->string('mName');
+            $table->string('lName');
+            $table->date('birthday');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
