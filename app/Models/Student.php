50960cf5b71e51a078c9154924785f99fa1cbf24
Angelu <200610313@my.xu.edu.ph>
2021-04-23 12:18:56 +0800

    Setup sanctum auth

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
 
 class Student extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $primaryKey = 'idNumber';
     public $incrementing = false;
     protected $fillable = ['idNumber', 'slmisNumber', 'sex', 'firstName', 'middleName', 'lastName', 'birthday'];
