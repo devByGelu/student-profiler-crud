46a52443e079513f8c0fd53f96e102a3c64c00e7
Angelu <200610313@my.xu.edu.ph>
2021-04-23 13:01:28 +0800

    protected login, logout, & register routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -14,9 +15,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::resource('students', StudentController::class);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 
