ad531e31d998cb605ee2cdbb562b274f622ab0aa
McKevin King <20150008204@my.xu.edu.ph>
2021-04-24 23:36:06 +0800

    Update StudentController.php
    Implement delete and update students

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -57,6 +57,26 @@ class StudentController extends Controller
     public function update(Request $request, $id)
     {
         //
+        if ($request->method() == 'PUT') {
+            $request->validate([
+                'idNumber' => ['required', 'integer', 'gt:0', 'unique:App\Models\Student,idNumber'], 'slmisNumber' => ['required', 'integer', 'gt:0', 'unique:App\Models\Student,slmisNumber'],
+                'sex' => ['required', 'in:Male,Female'], 'firstName' => ['required', 'alpha', 'between:2,20'],
+                'middleName' => ['required', 'alpha', 'between:2,20'], 'lastName' => ['required', 'alpha', 'between:2,20'], 'birthday' => ['required', 'date'],
+            ]);
+        } else {
+            $request->validate([
+                'idNumber' => ['sometimes', 'integer', 'gt:0', 'unique:App\Models\Student,idNumber'],
+                'slmisNumber' => ['sometimes', 'integer', 'gt:0', 'unique:App\Models\Student,slmisNumber'],
+                'sex' => ['sometimes', 'in:Male,Female'],
+                'firstName' => ['sometimes', 'alpha', 'between:2,20'],
+                'middleName' => ['sometimes', 'alpha', 'between:2,20'],
+                'lastName' => ['sometimes', 'alpha', 'between:2,20'],
+                'birthday' => ['sometimes', 'date'],
+            ]);
+        }
+        $student = Student::findOrFail($id);
+        $student->update($request->all());
+        return $student;
     }
 
     /**
@@ -68,5 +88,8 @@ class StudentController extends Controller
     public function destroy($id)
     {
         //
+        $student = Student::findOrFail($id);
+        $student->delete();
+        return $student;
     }
 }
