437df5e762ddb167954df5d8f94a781bb5d69343
McKevin King <200610313@my.xu.edu.ph>
2021-04-24 09:12:27 +0800

    Set up axios with sanctum

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,9 @@
 // import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import axios from "axios";
+
+axios.defaults.withCredentials = true;
 
 function MyApp({ Component, pageProps }) {
     return (
--- frontend/pages/auth/signin.tsx
+++ frontend/pages/auth/signin.tsx
@@ -1,5 +1,21 @@
+import { Button } from "@chakra-ui/react";
+import axios from "axios";
 import React from "react";
 
 export default function SignInPage() {
-    return <div></div>;
+    const handleClickSignIn = async (params) => {
+        await axios.get("http://localhost:8000/sanctum/csrf-cookie");
+        //localhost:8000/login
+        const res = await axios.post("http://localhost:8000/login", {
+            email: "",
+            password: "",
+        });
+        // console.log(res.data);
+    };
+
+    return (
+        <div>
+            <Button onClick={handleClickSignIn}>Sign in</Button>
+        </div>
+    );
 }
