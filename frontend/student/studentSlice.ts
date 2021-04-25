3bb7bed5db02439f5e51959c47946350b2c1c7ea
McKevin King <200610313@my.xu.edu.ph>
2021-04-25 20:43:52 +0800

    updated student slice

--- frontend/student/studentSlice.ts
+++ frontend/student/studentSlice.ts
@@ -15,11 +15,13 @@ export interface IStudent {
 
 interface StudentState {
     selected: null | IStudent;
+    mode: "update" | "create";
 }
 
 // Define the initial state using that type
 const initialState: StudentState = {
     selected: null,
+    mode: "update",
 };
 
 export const studentSlice = createSlice({
@@ -27,15 +29,27 @@ export const studentSlice = createSlice({
     // `createSlice` will infer the state type from the `initialState` argument
     initialState,
     reducers: {
-        select: (state, action: PayloadAction<IStudent | null>) => {
-            state.selected = action.payload;
+        select: (
+            state,
+            action: PayloadAction<{
+                student: IStudent;
+                mode: StudentState["mode"];
+            } | null>
+        ) => {
+            state.selected = action.payload.student;
+
+            state.mode = action.payload.mode;
+        },
+        setMode: (state, action: PayloadAction<StudentState["mode"]>) => {
+            state.mode = action.payload;
         },
     },
 });
 
-export const { select } = studentSlice.actions;
+export const { select, setMode } = studentSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
 export const selectStudent = (state: RootState) => state.student.selected;
+export const selectMode = (state: RootState) => state.student.mode;
 
 export default studentSlice.reducer;
