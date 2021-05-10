d430db55bdf58d5636bf25a1ecb3903144e10930
200610313@my.xu.edu.ph <200610313@my.xu.edu.ph>
2021-05-10 20:06:17 +0800

    added * to CORS paths

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','login','logout', 'home', 'user', 'register'],
+    'paths' => ['*','api/*', 'sanctum/csrf-cookie','login','logout', 'home', 'user', 'register'],
 
     'allowed_methods' => ['*'],
 
